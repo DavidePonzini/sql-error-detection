25e56ed577adfd141a48d004b3655d9ac8ff59a2
Davide Ponzini <davide.ponzini95@gmail.com>
2024-10-24 13:12:00 +0200

    can now read tables in db

---
+++ database.py
@@ -0,0 +1,161 @@
+from datetime import datetime
+
+from dav_tools import database, messages
+
+
+db = database.PostgreSQL(
+    database='postgres',
+    host='127.0.0.1',
+    user='sql_misconceptions_admin',
+    password='sql'
+)
+
+schema_prefix = 'sql_misconceptions'
+
+
+def setup_schema(create_tables_file: str) -> str:
+        with open(create_tables_file) as f:
+             create_tables = f.read()
+
+        '''Create a unique schema with the specified tables and return its name'''
+        now = datetime.now()
+        schema = f'{schema_prefix}_{now.strftime("%Y%m%d%H%M%S%f")}'
+
+        with db.connect() as c:
+            c.create_schema(schema)
+            messages.info(f'Created schema "{schema}"')
+            c.set_schema(schema)
+            c.execute(create_tables)
+            messages.info('Created tables')
+            
+            c.commit()
+
+        return schema
+
+def delete_schema(schema: str):
+    with db.connect() as c:
+        c.delete_schema(schema)
+        messages.info(f'Deleted schema "{schema}"')
+        
+        c.commit()
+
+def get_tables_in_schema(schema: str) -> list[tuple]:
+    query = database.sql.SQL(
+        '''
+            SELECT
+                table_name,
+                table_type
+            FROM
+                information_schema.tables
+            WHERE
+                table_schema = {schema};
+        ''').format(
+             schema=database.sql.Placeholder('schema'),
+        )
+
+    result = db.execute_and_fetch(query, {
+         'schema': schema,
+    })
+
+    return result
+
+def get_columns_in_table(schema: str, table: str) -> list[tuple]:
+    query = database.sql.SQL(
+        '''
+            SELECT
+                c.column_name,
+                c.data_type,
+                -- Is Nullable as Boolean
+                CASE WHEN c.is_nullable = 'YES' THEN TRUE ELSE FALSE END AS is_nullable,
+                -- Primary Key as Boolean
+                CASE WHEN tc_pk.constraint_type = 'PRIMARY KEY' THEN TRUE ELSE FALSE END AS is_primary_key,
+                -- Unique Constraint as Boolean
+                CASE WHEN tc_unique.constraint_type = 'UNIQUE' THEN TRUE ELSE FALSE END AS is_unique,
+                -- Foreign Key Table and Column
+                fk_info.foreign_table AS fk_table,
+                fk_info.foreign_column AS fk_column
+            FROM
+                information_schema.columns c
+                -- Primary Key
+                LEFT JOIN (
+                    SELECT
+                        kcu.table_schema,
+                        kcu.table_name,
+                        kcu.column_name,
+                        tc.constraint_type
+                    FROM
+                        information_schema.table_constraints tc
+                        JOIN information_schema.key_column_usage kcu
+                        ON tc.constraint_name = kcu.constraint_name
+                        AND tc.table_schema = kcu.table_schema
+                    WHERE
+                        tc.constraint_type = 'PRIMARY KEY'
+                ) tc_pk ON c.table_schema = tc_pk.table_schema
+                        AND c.table_name = tc_pk.table_name
+                        AND c.column_name = tc_pk.column_name
+                -- Unique Constraints
+                LEFT JOIN (
+                    SELECT
+                        kcu.table_schema,
+                        kcu.table_name,
+                        kcu.column_name,
+                        tc.constraint_type
+                    FROM
+                        information_schema.table_constraints tc
+                        JOIN information_schema.key_column_usage kcu
+                        ON tc.constraint_name = kcu.constraint_name
+                        AND tc.table_schema = kcu.table_schema
+                    WHERE
+                        tc.constraint_type = 'UNIQUE'
+                ) tc_unique ON c.table_schema = tc_unique.table_schema
+                        AND c.table_name = tc_unique.table_name
+                        AND c.column_name = tc_unique.column_name
+                -- Foreign Keys
+                LEFT JOIN (
+                    SELECT
+                        kcu.table_schema,
+                        kcu.table_name,
+                        kcu.column_name,
+                        ccu.table_name AS foreign_table,
+                        ccu.column_name AS foreign_column
+                    FROM
+                        information_schema.key_column_usage kcu
+                        JOIN information_schema.table_constraints tc
+                        ON kcu.constraint_name = tc.constraint_name
+                        AND kcu.table_schema = tc.table_schema
+                        JOIN information_schema.constraint_column_usage ccu
+                        ON ccu.constraint_name = tc.constraint_name
+                        AND ccu.table_schema = tc.table_schema
+                    WHERE
+                        tc.constraint_type = 'FOREIGN KEY'
+                ) fk_info ON c.table_schema = fk_info.table_schema
+                        AND c.table_name = fk_info.table_name
+                        AND c.column_name = fk_info.column_name
+            WHERE
+                c.table_schema = {schema}
+                AND c.table_name = {table}
+            ORDER BY
+                c.ordinal_position;
+
+        ''').format(
+            schema=database.sql.Placeholder('schema'),
+            table=database.sql.Placeholder('table'),
+        )
+
+    result = db.execute_and_fetch(query, {
+         'schema': schema,
+         'table': table
+    })
+
+    return [{
+        'column_name':      row[0],
+        'data_type':        row[1],
+        'is_nullable':      row[2],
+        'is_primary_key':   row[3],
+        'is_unique':        row[4],
+        'fk_table':         row[5],
+        'fk_column':        row[6],
+    } for row in result]
+ 
+
+    
--- query_create.py
+++ query_create.py
@@ -1,36 +1,36 @@
-from sqlparse.sql import Identifier, IdentifierList, Parenthesis
-from sqlparse import tokens as ttypes
-
-from query import Query
-import util
-
-from dav_tools import messages          # debugging
-
+import database
 
 class TableColumn:
-    def __init__(self, name, dtype, /,
-                 is_primary_key: bool = False, is_unique: bool = False,
-                 foreign_key_column = None):
-        self.name = name
+    def __init__(self, name: str, dtype: str):
+        self.name: str = name
         self.dtype = dtype
         self.table = None
-        self.is_primary_key = is_primary_key
-        self.is_unique = is_unique
-        self.foreign_key_column = foreign_key_column
+        self.is_primary_key: bool = False
+        self.is_unique: bool = False
+        self.is_nullable: bool = False
+        self.foreign_key_table: str = None
+        self.foreign_key_column: str = None
 
     @property
     def is_foreign_key(self):
-        return self.foreign_key_column is not None
+        return self.foreign_key_table is not None and self.foreign_key_column is not None
+
+    def add_to_table(self, table):
+        return table.add_column(self)
 
     def __repr__(self):
-        return f'|{self.name}|{"(PK)" if self.is_primary_key else ""}{"(FK)" if self.is_foreign_key else ""}{"(UQ)" if self.is_unique else ""}'
+        modifiers = ''
+        if self.is_primary_key:
+            modifiers += '*'
+        if self.is_foreign_key:
+            modifiers += '^'
+        return self.name + modifiers
 
 
 class Table:
-    def __init__(self, real_name):
-        self.real_name = real_name
-        self.name = real_name
-        self.columns = []
+    def __init__(self, name):
+        self.name: str = name
+        self.columns: list[TableColumn] = []
 
     def add_column(self, column: TableColumn):
         assert column.table is None, 'Column is already associated to another table'
@@ -45,120 +45,34 @@ class Table:
         return None
 
     def __repr__(self):
-        return f'Table <{self.name} ({self.real_name})> : {self.columns})'
+        return f'{self.name}({self.columns})'
 
 
-class CreateQuery(Query):
-    def __init__(self, query_text):
-        super().__init__(query_text)
+def get_tables(filepath: str) -> list[Table]:
+    tables = {}
+    
+    # create the tables on a temporary schema
+    schema = database.setup_schema(filepath)
+    tables_list = database.get_tables_in_schema(schema)
 
-        assert self.tokens[0].ttype is ttypes.DDL
+    for table_name, table_type in tables_list:
+        table = Table(table_name)
+        tables[table_name] = table
 
-    def extract_tables(self):
-        tables = []
-        current_table = None
+        columns = database.get_columns_in_table(schema, table_name)
+        for column in columns:
+            new_col = TableColumn(column['column_name'], column['data_type'])
 
-        tokens = iter(self.tokens)
-        while True:
-            try:
-                # CREATE
-                token = next(tokens)
-                assert token.ttype is ttypes.DDL, f'{token} is not DDL'
+            new_col.is_nullable         = column['is_nullable']
+            new_col.is_primary_key      = column['is_primary_key']
+            new_col.is_unique           = column['is_unique']
+            new_col.foreign_key_table   = column['fk_table']
+            new_col.foreign_key_column  = column['fk_column']
 
-                # TABLE
-                token = next(tokens)
-                assert token.ttype is ttypes.Keyword and token.value.upper() == 'TABLE', f'{token} is not TABLE'
+            new_col.add_to_table(table)
 
-                # table_name
-                token = next(tokens)
-                assert isinstance(token, Identifier), f'{token} is not Identifier'
-                current_table = Table(token.get_name())
-                tables.append(current_table)
+    database.delete_schema(schema)
 
-                # columns
-                token = next(tokens)
-                assert isinstance(token, Parenthesis), f'{token} is not Parenthesis'
-
-                column_tokens = iter(util.strip_spaces_and_comments(token))
-                while True:
-                    try:
-                        collecting_column = False
-                        collecting_constraint = False
-                        col_token = next(column_tokens)
-                        messages.info(col_token)
-
-                        if col_token.ttype is ttypes.Punctuation and col_token.value == ',':   # comma, look for next column
-                            messages.error(col_token)
-                            collecting_constraint = False
-                            collecting_column = False
-                            continue
-
-                        if not collecting_column and not collecting_constraint and isinstance(col_token, Identifier):
-                            # Column definition or constraint
-                            messages.warning(col_token)
-
-                            # TODO: understand if it's a column or a constraint
-
-
-
-
-
-
-
-                #             col_name = col_token.get_name()
-                #             dtype_token = next(column_tokens)
-                #             col_dtype = dtype_token.value
-
-                #             # Optional column constraints
-                #             is_primary_key = False
-                #             is_unique = False
-                #             is_foreign_key = False
-                #             foreign_table = None
-                #             foreign_key_column = None
-
-                #             while True:
-                #                 constraint_token = next(column_tokens)
-                #                 if constraint_token.ttype is Keyword:
-                #                     messages.warning(f'{current_table.name} - {constraint_token}')
-                #                     if constraint_token.value.upper() == 'PRIMARY':
-                #                         is_primary_key = True
-                #                         # Skip "KEY"
-                #                         next(column_tokens)
-                #                     elif constraint_token.value.upper() == 'UNIQUE':
-                #                         is_unique = True
-                #                     elif constraint_token.value.upper() == 'REFERENCES':
-                #                         messages.warning('ref')
-                #                         is_foreign_key = True
-                #                         c = next(column_tokens)
-                #                         messages.warning(repr(c))
-                #                         foreign_table = c.get_name()
-                #                         foreign_key_column = next(column_tokens).get_name()
-
-                #                 if constraint_token.ttype is ttypes.Punctuation and constraint_token.value == ',':
-                #                     break
-
-                #             # Add the column to the current table
-                #             current_table.add_column(
-                #                 TableColumn(col_name, col_dtype, current_table.name,
-                #                             is_primary_key=is_primary_key,
-                #                             is_unique=is_unique,
-                #                             is_foreign_key=is_foreign_key,
-                #                             foreign_key_name=foreign_table,
-                #                             foreign_key_column=foreign_key_column)
-                #             )
-
-                #             messages.warning(f'added column {current_table.name}.{col_name}')
-
-                    except StopIteration:
-                        break  # End of columns
-
-                # trailing semicolon
-                token = next(tokens)
-                assert token.ttype is ttypes.Punctuation, f'{token} is not Semicolon'
-
-            except StopIteration:
-                break
-
-        return tables
+    return tables
 
 
